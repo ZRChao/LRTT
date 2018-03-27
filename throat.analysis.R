163c1a9134d31dc99a47fbc7952b8d4a5a07ace8
ZRChao <Supdream8@sjtu.edu.cn>
2018-03-27 15:22:45 +0800

    Update throat.analysis.R

--- throat.analysis.R
+++ throat.analysis.R
@@ -9,9 +9,7 @@ rm(list = ls())
 library(MiSPU)
 library(ancom.R)
 library(metagenomeSeq)
-source("~/Rcode/Tree.ratio.R")
-source("~/Rcode/Taxa.index.R")
-source("~/Rcode/NonTree.test.R")
+library(LRTT)
 source("~/Rcode/Zig.pv.adj.R")
 data("throat.otu.tab")
 data("throat.meta")
@@ -70,12 +68,6 @@ throat.ancom.py.result <- read.table("/Users/zhouchao/Documents/Tree/throat/anco
 throat.ancom.py.delected <- which(throat.ancom.py.result[, 3] == "True")
 
 
-###--------------- NonTree take the reject proportion to do decesion of p-1 otu ------------------###
-## Although ancom.R::ANCOM use the parallel technolongy in R but it is still very slow, nontree ratio
-## test is just the same with it. (this spend about 2.4 min)
-throat.nontree.result <- NonTree_test(throat.otu.tab, p = otu.number, group = throat.group, log = T)
-throat.nontree.detected <- which(throat.nontree.result[, 3] > 0.5)
-
 ###---------------------------- Tree Ratio test with correction ----------------------------------###
 throat.taxa.index <- Taxa_index(p = otu.number, phy_tree = throat.tree)
 throat.taxa.table <- as.matrix(throat.otu.tab)%*%throat.taxa.index
@@ -90,63 +82,7 @@ throat.tree.ratio.detected <- names(which(throat.tree.back.adj < 0.1))
 
 
 ###------------finally we can get the differential OTU detected by this methods--------------------###
-## we can plot venn plot which show that the tree.ratio test can find the most OTU which content the
-## OTU detected by ANCOM while t.test and zig have no detected or just 1. However, we can use the FWER
+## we can use the FWER
 ## (Family-Wise Error Rate) to compare the type I error of this methods. So, we permutate the label of
-## the Smoking Status 1000 times to calculate it as follow.
-
-L <- 100
-throat.FWER <- matrix(NA, nrow = L, ncol = 5)
-colnames(throat.FWER) <- c("t.test", "Zig", "NonTree", "ANCOM", "Tree.Ratio")
-throat.grouping.permt.label <- matrix(1, nrow = L, ncol = nrow(throat.meta))
-
-start.permt = Sys.time()
-for(i in 1:L){
-  set.seed(i)
-  throat.grouping.permt <- sample(1:nrow(throat.meta), 28)
-  throat.grouping.permt.label[i, throat.grouping.permt] <- 2
-
-  # t.test
-  throat.t.rel.pv.permt <- apply(throat.log.rel.data, 2, function(x)
-    return(ifelse(length(unique(x)) == 1, 1, t.test(x[throat.grouping.permt], x[-throat.grouping.permt])$p.value)))
-  throat.t.rel.p.adj.permt <- p.adjust(throat.t.rel.pv.permt, method = "fdr", n = otu.number)
-  throat.FWER[i, 1] <- sum(throat.t.rel.p.adj.permt <= 0.05)
-
-  # zig, here functional the zig process
-  throat.zig.p.adj.permt <- Zig.pv.adj(throat.otu.tab, group = throat.grouping.permt.label[i, ])
-  throat.FWER[i, 2] <- sum(throat.zig.p.adj.permt[-which(is.na(throat.zig.p.adj.permt))] <= 0.05)
-
-  # nontree
-  throat.nontree.rp.permt <- NonTree.test(throat.otu.tab, p = otu.number,
-                                              group = throat.grouping.permt.label[i, ], log = T)[, 3]
-  throat.FWER[i, 3] <- sum(throat.nontree.rp.permt >= 0.5)
-
-  # tree.ratio
-  throat.tree.ratio.permt <- Tree.ratio(p = otu.number, tree = throat.tree, all_table = throat.all.table,
-                                  group = throat.grouping.permt, taxa_index = throat.taxa.index)
-  throat.tree.ratio.pv.permt <- Tree.ratio.back(p = otu.number, tree_ratio = throat.tree.ratio.permt,
-                                      otutab = throat.otu.tab, taxa_index = throat.taxa.index, group = throat.grouping.permt)
-  throat.FWER[i, 5] <- sum(throat.tree.ratio.pv.permt <= 0.05)
-
-}
-Sys.time() - start
-
-# use ancomP in python which is more faster than in R
-write.table(throat.grouping.permt.label, "throat.grouping.permt.label.txt", row.names = F, col.names = F)
-ancom.permt.result <- read.table("ancom.permt.result.csv", sep = ",")
-throat.FWER[, 4] <- apply(ancom.permt.result[seq(3, (3*L), 3), -1], 1, function(x)
-                                                      return(ifelse(sum(x == "True") != 0, 1, 0)))
-
-write.table(throat.FWER, "throat.FWER.txt", col.names = T, row.names = F)
-
-throat.FWER[throat.FWER != 0] <- 1
-print(colMeans(throat.FWER))
-
-setwd("/Users/zhouchao/Documents/Tree/throat"); getwd()
-save.image(file = "throat.RData")
-
-
-
-
-
-
+## the Smoking Status 1000 times to calculate it, which show that ANCOM always have some detected which 
+## is the type I error, so we would like to infrom you be careful to the results from ANCOM.
